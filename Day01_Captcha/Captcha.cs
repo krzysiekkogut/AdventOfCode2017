180f6e18ce4e5a7e34c513a1a420918f603cc5c0
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2017-12-01 19:22:56 +0100

    Day 01 puzzle solution - Captcha

---
+++ Day01_Captcha/Captcha.cs
@@ -0,0 +1,22 @@
+﻿namespace Day01_Captcha
+{
+    public class Captcha
+    {
+        public static int Solve(string input)
+        {
+            var result = 0;
+            for (var currentPosiion = 0; currentPosiion < input.Length; currentPosiion++)
+            {
+                var nextPosition = (currentPosiion + 1) % input.Length;
+                var currentDigit = int.Parse(input[currentPosiion].ToString());
+                var nextDigit = int.Parse(input[nextPosition].ToString());
+                if (currentDigit == nextDigit)
+                {
+                    result += currentDigit;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PuzzlesRunner/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace PuzzlesRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayNumber = args[0];
+            var input = args[1];
+            switch (dayNumber)
+            {
+                case "01":
+                    WriteLine(Day01_Captcha.Captcha.Solve(input));
+                    break;
+                default:
+                    WriteLine($"Day '{dayNumber}' is not yet solved.");
+                    break;
+            }
+        }
+    }
+}
